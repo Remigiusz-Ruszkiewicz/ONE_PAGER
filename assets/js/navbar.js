383b5531174d8c5c296020a3ffa42c30b72eb807
Remik <47605818+jagoda2309@users.noreply.github.com>
2021-07-15 21:49:39 +0200

    Website

---
+++ assets/js/navbar.js
@@ -0,0 +1,63 @@
+function chcl1() {
+    document.getElementById('1').style.color = ("#2ee6e0") ;
+    document.getElementById('2').style.color = ("black") ;
+    document.getElementById('3').style.color = ("black") ;
+    document.getElementById('4').style.color = ("black") ;
+    document.getElementById('5').style.color = ("black") ;
+    document.getElementById('6').style.color = ("black") ;
+    document.getElementById('7').style.color = ("black") ;
+}
+function chcl2() {
+    document.getElementById('1').style.color = ("black") ;
+    document.getElementById('2').style.color = ("#2ee6e0") ;
+    document.getElementById('3').style.color = ("black") ;
+    document.getElementById('4').style.color = ("black") ;
+    document.getElementById('5').style.color = ("black") ;
+    document.getElementById('6').style.color = ("black") ;
+    document.getElementById('7').style.color = ("black") ;
+}
+function chcl3() {
+    document.getElementById('1').style.color = ("black") ;
+    document.getElementById('2').style.color = ("black") ;
+    document.getElementById('3').style.color = ("#2ee6e0") ;
+    document.getElementById('4').style.color = ("black") ;
+    document.getElementById('5').style.color = ("black") ;
+    document.getElementById('6').style.color = ("black") ;
+    document.getElementById('7').style.color = ("black") ;
+}
+function chcl4() {
+    document.getElementById('1').style.color = ("black") ;
+    document.getElementById('2').style.color = ("black") ;
+    document.getElementById('3').style.color = ("black") ;
+    document.getElementById('4').style.color = ("#2ee6e0") ;
+    document.getElementById('5').style.color = ("black") ;
+    document.getElementById('6').style.color = ("black") ;
+    document.getElementById('7').style.color = ("black") ;
+}
+function chcl5() {
+    document.getElementById('1').style.color = ("black") ;
+    document.getElementById('2').style.color = ("black") ;
+    document.getElementById('3').style.color = ("black") ;
+    document.getElementById('4').style.color = ("black") ;
+    document.getElementById('5').style.color = ("#2ee6e0") ;
+    document.getElementById('6').style.color = ("black") ;
+    document.getElementById('7').style.color = ("black") ;
+}
+function chcl6() {
+    document.getElementById('1').style.color = ("black") ;
+    document.getElementById('2').style.color = ("black") ;
+    document.getElementById('3').style.color = ("black") ;
+    document.getElementById('4').style.color = ("black") ;
+    document.getElementById('5').style.color = ("black") ;
+    document.getElementById('6').style.color = ("#2ee6e0") ;
+    document.getElementById('7').style.color = ("black") ;
+}
+function chcl7() {
+    document.getElementById('1').style.color = ("black") ;
+    document.getElementById('2').style.color = ("black") ;
+    document.getElementById('3').style.color = ("black") ;
+    document.getElementById('4').style.color = ("black") ;
+    document.getElementById('5').style.color = ("black") ;
+    document.getElementById('6').style.color = ("black") ;
+    document.getElementById('7').style.color = ("#2ee6e0") ;
+}
